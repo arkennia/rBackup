cb0eecb3bc270f6b2b32a74077314e7aa9b2e824
Jonathan <jonathan.manly@wsu.edu>
2020-05-04 23:56:36 -0700

    Working on the code for creating rsync commands from the UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,8 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "utility.h"
 
-MainWindow::MainWindow(QWidget *parent)
-        : QMainWindow(parent)
-        , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
         ui->setupUi(this);
 }
@@ -13,3 +12,83 @@ MainWindow::~MainWindow()
         delete ui;
 }
 
+void MainWindow::on_browseDest_clicked()
+{
+        QString fileName = QFileDialog::getExistingDirectory(
+                this, tr("Select Directory"), "/home",
+                QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+
+        ui->destination->setText(fileName);
+}
+
+void MainWindow::on_browseSrc_clicked()
+{
+        QString fileName = QFileDialog::getExistingDirectory(
+                this, tr("Select Directory"), "/home",
+                QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+
+        ui->destination->setText(fileName);
+}
+
+void MainWindow::on_generateButton_clicked()
+{
+}
+
+QString MainWindow::generate() const
+{
+        QString out = selectBackupType();
+
+        if (ui->transferCompression->isChecked())
+                out += TRANSFER_COMPRESSION;
+
+        out += selectDeleteType();
+
+        return out;
+}
+
+QString MainWindow::selectBackupType() const
+{
+        QString out = "";
+
+        switch (ui->backupType->currentIndex()) {
+        case 0:
+                out += INCREMENTAL_OPTIONS;
+                break;
+        case 1:
+                out += INCREMENTAL_OPTIONS;
+                out += NO_DELTA;
+                break;
+        case 2:
+                out += FULL_OPTIONS;
+                break;
+        case 3:
+                out += INCREMENTAL_OPTIONS;
+                out += NO_DELTA;
+                break;
+        default:
+                throw std::out_of_range("Invalid Backup Type Index");
+        }
+
+        return out;
+}
+
+QString MainWindow::selectDeleteType() const
+{
+        QString out = "";
+
+        switch (ui->deleteWhen->currentIndex()) {
+        case 0:
+                out += DELETE_DURING;
+                break;
+        case 1:
+                out += DELETE_AFTER;
+                break;
+        case 2:
+                out += DELETE_BEFORE;
+                break;
+        default:
+                throw std::out_of_range("Invalid Delete Type Index");
+        }
+
+        return out;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,21 +1,36 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QFileDialog>
 #include <QMainWindow>
+#include <stdexcept>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+        class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
         Q_OBJECT
 
-public:
+    public:
         MainWindow(QWidget *parent = nullptr);
         ~MainWindow();
 
-private:
+    private slots:
+        void on_browseDest_clicked();
+        void on_browseSrc_clicked();
+
+        void on_generateButton_clicked();
+
+    private:
         Ui::MainWindow *ui;
+
+        QString generate() const;
+        QString selectBackupType() const;
+        QString selectDeleteType() const;
 };
 #endif // MAINWINDOW_H
